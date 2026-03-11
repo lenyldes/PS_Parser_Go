1bffa70e9f71cab38de90f3f048a295a09081627
test <test>
2026-03-11 13:40:03 +0300

    ADD: readFromJSON

--- json_utils.go
+++ json_utils.go
@@ -5,16 +5,17 @@ import (
 	"os"
 )
 
+type Game struct {
+	Name          string `json:"name"`
+	Link          string `json:"link"`
+	ImgURL        string `json:"imgURL"`
+	OfferPrice    string `json:"offerPrice"`
+	OriginalPrice string `json:"originalPrice"`
+	ProductType   string `json:"productType"`
+}
+
 // saveToJSON сохраняет результат в JSON-файл
 func saveToJSON(resultMap map[string]map[string]string, filename string) error {
-	type Game struct {
-		Name          string `json:"name"`
-		Link          string `json:"link"`
-		ImgURL        string `json:"imgURL"`
-		OfferPrice    string `json:"offerPrice"`
-		OriginalPrice string `json:"originalPrice"`
-		ProductType   string `json:"productType"`
-	}
 
 	var games []Game
 	for name, data := range resultMap {
@@ -37,4 +38,32 @@ func saveToJSON(resultMap map[string]map[string]string, filename string) error {
 	encoder := json.NewEncoder(file)
 	encoder.SetIndent("", "  ") // форматирование с отступами
 	return encoder.Encode(games)
+}
+
+// readFromJSON читает данные из JSON-файла и возвращает карту с данными об играх
+func readFromJSON(filename string) (map[string]map[string]string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var games []Game
+	err = json.NewDecoder(file).Decode(&games)
+	if err != nil {
+		return nil, err
+	}
+
+	resultMap := make(map[string]map[string]string)
+	for _, game := range games {
+		resultMap[game.Name] = map[string]string{
+			"link":          game.Link,
+			"imgURL":        game.ImgURL,
+			"offerPrice":    game.OfferPrice,
+			"originalPrice": game.OriginalPrice,
+			"productType":   game.ProductType,
+		}
+	}
+
+	return resultMap, nil
 }
